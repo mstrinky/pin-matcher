2cee65cf6645524dd5c211ab3a62f401d40acc2c
Mst Rinky Akter <mstrinkya@gmail.com>
2022-10-24 17:11:48 +0600

    repo complete

--- pin-matcher-master/app.js
+++ pin-matcher-master/app.js
@@ -14,7 +14,6 @@ function generatePin(){
 //    console.log(pin);
 document.getElementById('display-pin').value = pin;
 }
-
 document.getElementById('key-pad').addEventListener('click',function(event){
     const calcInput = document.getElementById('typed-numbers');
   const number = event.target.innerText;
@@ -24,23 +23,19 @@ document.getElementById('key-pad').addEventListener('click',function(event){
   if (number=='C'){
    calcInput.value='';
   }
-
-  else{
-  
+ else{
   const previousNumber = calcInput.value;
   const newNumber = previousNumber + number;
   calcInput.value =newNumber;
   }
 })
-
 function verifyPin(){
    // console.log('going to verify');
    const pin = document.getElementById('display-pin').value;
    const typeNumbers = document.getElementById('typed-numbers').value;
  const successMessage = document.getElementById('notify-success');
 const error = document.getElementById('notify-fail');
-
-   if(pin==typeNumbers){
+if(pin==typeNumbers){
       // console.log('matched.YAY');
      successMessage.style.display='block';
      error.style.display='none';
